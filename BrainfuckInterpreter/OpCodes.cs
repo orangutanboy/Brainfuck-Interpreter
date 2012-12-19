0626da8c64c9f098509c4b0889ba9a5457fd16a6
Anthony Brown <bruinbrown@gmail.com>
2012-12-19 00:48:31 +0000

    Added opcodes and memory nodes class

---
+++ BrainfuckInterpreter/OpCodes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrainfuckInterpreter
+{
+    enum OpCode : byte
+    {
+        IncrementPointer = (byte)'>',
+        DecrementPointer = (byte)'<',
+        IncrementValue = (byte)'+',
+        DecrementValue = (byte)'-',
+        OutputValue = (byte)'.',
+        InputValue = (byte)',',
+        BeginLoop = (byte)'[',
+        EndLoop = (byte)']'        
+    }
+}
