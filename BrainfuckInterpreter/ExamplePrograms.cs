7bd596211195b1afa44ffec62a9c37d5d0513277
Anthony Brown <bruinbrown@gmail.com>
2012-12-19 16:00:58 +0000

    Added some example programs

---
+++ BrainfuckInterpreter/ExamplePrograms.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrainfuckInterpreter
+{
+    public static class ExamplePrograms
+    {
+        /// <summary>
+        /// Simply prints Hello World! to the console
+        /// </summary>
+        public const string HelloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-] >++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        /// <summary>
+        /// Takes a user input and subtracts 32 from it. If the value is the enter key then the program exits
+        /// </summary>
+        public const string LowerToUpper = ",-------------[-------------------.,-------------]";
+    }
+}
